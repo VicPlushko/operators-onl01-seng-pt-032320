4247699e264330fb7d72ff1eb87f80c340ebc9a0
Victor Plushko <vicplushko33@gmail.com>
2020-03-26 03:42:30 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 def unsafe?(speed)
 if speed < 40 || speed > 60
   return true
@@ -9,7 +11,7 @@ end
 
 
 def not_safe?(speed)
-  speed < 40 || > 60 ? "true" : "false"
+  speed < 40 || > 60 ? true : false
 end 
 	
 
